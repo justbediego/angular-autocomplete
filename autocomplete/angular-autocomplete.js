fbfae9d579078d546c5e9d5e66eab9a1bcede453
Hossein <Hossein@HOSSEIN-I3>
2017-12-27 03:09:35 +0330

    going on

--- autocomplete/angular-autocomplete.js
+++ autocomplete/angular-autocomplete.js
@@ -15,11 +15,15 @@ app.directive("autoComplete", function () {
             ngKey: '=',
             ngValue: '='
         },
-        template: "<input type='text' style='height: {{ngHeight}}; width: {{ngWidth}}; direction: {{ngDirection}}; font-family:{{ngFontfamily}}; font-size:{{ngFontsize}}' placeholder={{ngPlaceholder}} ng-change='textChanged()' ng-model='tempText'/>",
+        template: "<div class='angular-autocomplete'><input type='text' style='height: {{ngHeight}}; width: {{ngWidth}}; direction: {{ngDirection}}; font-family:{{ngFontfamily}}; font-size:{{ngFontsize}}' placeholder={{ngPlaceholder}} ng-change='textChanged()' ng-model='tempText'/><div class='suggestions' style='width:{{ngWidth}}'><div class='border'><div ng-repeat='match in scope.matches' class='suggestion'>{{match.value}}</div></div></div></div>",
         link: function (scope, element, attrs) {
             scope.tempText = scope.ngValue;
+            scope.matches = [];
             scope.textChanged = function () {
-
+                if (scope.tempText.length >= scope.ngMinchar) {
+                    scope.matches = scope.ngSearchMethod()(scope.tempText);
+                    debugger;
+                }
             }
         }
     }
--- sample/mainCtrl.js
+++ sample/mainCtrl.js
@@ -1,6 +1,36 @@
 angular.module("autocompleteDemo").controller('mainCtrl', function ($scope) {
+    var data = [
+        {
+            key: 12,
+            value: 'First Test'
+        },
+        {
+            key: 13,
+            value: 'First Love'
+        },
+        {
+            key: 14,
+            value: 'First Date'
+        },
+        {
+            key: 15,
+            value: 'First Breakup'
+        },
+        {
+            key: 16,
+            value: 'First Time'
+        },
+        {
+            key: 17,
+            value: 'First Name'
+        },
+        {
+            key: 18,
+            value: 'First Test'
+        },
+    ]
     $scope.DemoMethod = function (phrase) {
-        debugger;
+        return data.filter(o => o.value.indexOf(phrase) == 0);
     };
     $scope.Key = 12;
     $scope.Value = "MASHTI OSKOL";
